e50a7315fa25da441314a7823fab8f2af5147692
jdchoi77 <jdchoi77@gmail.com>
2012-09-07 21:50:51 -0400

    Version 1.0.0.

--- src/main/scripts/rsync.sh
+++ src/main/scripts/rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-jar cf clearnlp-$1.jar edu
-rsync -avc clearnlp-$1.jar choijd@verbs.colorado.edu:/data/choijd/opt/clearnlp/lib
+jar cf clearnlp-$1.jar com
+rsync -avc clearnlp-$1.jar jdchoi@blake.cs.umass.edu:/home/jdchoi/clearnlp/lib
