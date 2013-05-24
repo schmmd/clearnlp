479f1bd99a02103ee2e23f6718c551b6a7023bbb
Michael Schmitz <michael@schmitztech.com>
2013-05-23 19:56:15 -0700

    Fix pos tagger.

--- src/main/java/com/googlecode/clearnlp/component/pos/CPOSTagger.java
+++ src/main/java/com/googlecode/clearnlp/component/pos/CPOSTagger.java
@@ -1,12 +1,12 @@
 /**
 * Copyright 2012 University of Massachusetts Amherst
-* 
+*
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
-* 
+*
 *   http://www.apache.org/licenses/LICENSE-2.0
-*   
+*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,6 +34,7 @@ import com.googlecode.clearnlp.component.AbstractStatisticalComponent;
 import com.googlecode.clearnlp.dependency.DEPNode;
 import com.googlecode.clearnlp.dependency.DEPTree;
 import com.googlecode.clearnlp.engine.EngineProcess;
+import com.googlecode.clearnlp.feature.xml.FtrTemplate;
 import com.googlecode.clearnlp.feature.xml.FtrToken;
 import com.googlecode.clearnlp.feature.xml.JointFtrXml;
 import com.googlecode.clearnlp.nlp.NLPLib;
@@ -55,15 +56,15 @@ public class CPOSTagger extends AbstractStatisticalComponent
 	private final String ENTRY_FEATURE		 = NLPLib.MODE_POS + NLPLib.ENTRY_FEATURE;
 	private final String ENTRY_LEXICA		 = NLPLib.MODE_POS + NLPLib.ENTRY_LEXICA;
 	private final String ENTRY_MODEL		 = NLPLib.MODE_POS + NLPLib.ENTRY_MODEL;
-	
+
 	protected final int LEXICA_LOWER_SIMPLIFIED_FORMS = 0;
 	protected final int LEXICA_AMBIGUITY_CLASSES      = 1;
-	
+
 	protected String[]          	g_tags;		// gold-standard part-of-speech tags
 	protected Set<String>			s_lsfs;		// lower simplified forms
 	protected Prob2DMap				p_ambi;		// ambiguity classes (only for collecting)
 	protected Map<String,String>	m_ambi;		// ambiguity classes
-	
+
 //	====================================== CONSTRUCTORS ======================================
 
 	/** Constructs a part-of-speech tagger for collecting lexica. */
@@ -74,34 +75,34 @@ public class CPOSTagger extends AbstractStatisticalComponent
 		s_lsfs = sLsfs;
 		p_ambi = new Prob2DMap();
 	}
-	
+
 	/** Constructs a part-of-speech tagger for training. */
 	public CPOSTagger(JointFtrXml[] xmls, StringTrainSpace[] spaces, Object[] lexica)
 	{
 		super(xmls, spaces, lexica);
 	}
-	
+
 	/** Constructs a part-of-speech tagger for developing. */
 	public CPOSTagger(JointFtrXml[] xmls, StringModel[] models, Object[] lexica)
 	{
 		super(xmls, models, lexica);
 	}
-	
+
 	/** Constructs a part-of-speech tagger for decoding. */
 	public CPOSTagger(ZipInputStream in)
 	{
 		super(in);
 	}
-	
+
 	@Override @SuppressWarnings("unchecked")
 	protected void initLexia(Object[] lexica)
 	{
 		s_lsfs = (Set<String>)lexica[LEXICA_LOWER_SIMPLIFIED_FORMS];
 		m_ambi = (Map<String,String>)lexica[LEXICA_AMBIGUITY_CLASSES];
 	}
-	
+
 //	====================================== LOAD/SAVE MODELS ======================================
-	
+
 	@Override
 	public void loadModels(ZipInputStream zin)
 	{
@@ -110,13 +111,13 @@ public class CPOSTagger extends AbstractStatisticalComponent
 		s_models = null;
 		ZipEntry zEntry;
 		String   entry;
-				
+
 		try
 		{
 			while ((zEntry = zin.getNextEntry()) != null)
 			{
 				entry = zEntry.getName();
-				
+
 				if      (entry.equals(ENTRY_CONFIGURATION))
 					loadDefaultConfiguration(zin);
 				else if (entry.startsWith(ENTRY_FEATURE))
@@ -125,16 +126,16 @@ public class CPOSTagger extends AbstractStatisticalComponent
 					loadStatisticalModels(zin, Integer.parseInt(entry.substring(mLen)));
 				else if (entry.equals(ENTRY_LEXICA))
 					loadLexica(zin);
-			}		
+			}
 		}
 		catch (Exception e) {e.printStackTrace();}
 	}
-	
+
 	private void loadLexica(ZipInputStream zin) throws Exception
 	{
 		BufferedReader fin = UTInput.createBufferedReader(zin);
 		System.out.println("Loading lexica.");
-		
+
 		s_lsfs = UTInput.getStringSet(fin);
 		m_ambi = UTInput.getStringMap(fin, " ");
 	}
@@ -152,50 +153,50 @@ public class CPOSTagger extends AbstractStatisticalComponent
 		}
 		catch (Exception e) {e.printStackTrace();}
 	}
-	
+
 	private void saveLexica(ZipOutputStream zout) throws Exception
 	{
 		zout.putNextEntry(new ZipEntry(ENTRY_LEXICA));
 		PrintStream fout = UTOutput.createPrintBufferedStream(zout);
 		System.out.println("Saving lexica.");
-		
+
 		UTOutput.printSet(fout, s_lsfs);		fout.flush();
 		UTOutput.printMap(fout, m_ambi, " ");	fout.flush();
-		
+
 		zout.closeEntry();
 	}
-	
+
 //	====================================== GETTERS AND SETTERS ======================================
 
 	@Override
 	public Object[] getLexica()
 	{
 		Object[] lexica = new Object[2];
-		
+
 		lexica[LEXICA_LOWER_SIMPLIFIED_FORMS] = s_lsfs;
 		lexica[LEXICA_AMBIGUITY_CLASSES] = (i_flag == FLAG_LEXICA) ? getAmbiguityClasses() : m_ambi;
-		
+
 		return lexica;
 	}
-	
+
 	@Override
 	public Object[] getGoldTags()
 	{
 		return g_tags;
 	}
-	
+
 	/** {@link AbstractStatisticalComponent#FLAG_LEXICA}. */
 	public Set<String> getLowerSimplifiedForms()
 	{
 		return s_lsfs;
 	}
-	
+
 	/** {@link AbstractStatisticalComponent#FLAG_LEXICA}. */
 	public void clearLowerSimplifiedForms()
 	{
 		s_lsfs.clear();
 	}
-	
+
 	/** Called by {@link CPOSTagger#getLexica()}. */
 	private Map<String,String> getAmbiguityClasses()
 	{
@@ -203,45 +204,45 @@ public class CPOSTagger extends AbstractStatisticalComponent
 		Map<String,String> mAmbi = new HashMap<String,String>();
 		StringDoublePair[] ps;
 		StringBuilder build;
-		
+
 		for (String key : p_ambi.keySet())
 		{
 			build = new StringBuilder();
 			ps = p_ambi.getProb1D(key);
 			Arrays.sort(ps);
-			
+
 			for (StringDoublePair p : ps)
 			{
 				if (p.d <= threshold)	break;
-				
+
 				build.append(AbstractColumnReader.BLANK_COLUMN);
 				build.append(p.s);
 			}
-			
+
 			if (build.length() > 0)
-				mAmbi.put(key, build.substring(1));				
+				mAmbi.put(key, build.substring(1));
 		}
-		
+
 		return mAmbi;
 	}
-	
+
 	@Override
 	public void countAccuracy(int[] counts)
 	{
 		int i, correct = 0;
-		
+
 		for (i=1; i<t_size; i++)
 		{
 			if (d_tree.get(i).pos.equals(g_tags[i]))
 				correct++;
 		}
-		
+
 		counts[0] += t_size - 1;
 		counts[1] += correct;
 	}
-	
+
 //	====================================== PROCESS ======================================
-	
+
 	@Override
 	public void process(DEPTree tree)
 	{
@@ -250,14 +251,14 @@ public class CPOSTagger extends AbstractStatisticalComponent
 		state.init(tree);
 		state.processAux();
 	}
-	
+
 	class State {
 	protected DEPTree				d_tree;
-	
+
 	protected int 					i_input;
 
 	protected int 					t_size;		// size of d_tree
-	
+
 	/** Called by {@link CPOSTagger#process(DEPTree)}. */
 	protected void init(DEPTree tree)
 	{
@@ -269,17 +270,17 @@ public class CPOSTagger extends AbstractStatisticalComponent
 	 		g_tags = tree.getPOSTags();
 	 		tree.clearPOSTags();
 	 	}
-	 	
+
 	 	EngineProcess.normalizeForms(tree);
 	}
-	
+
 	/** Called by {@link CPOSTagger#process(DEPTree)}. */
 	protected void processAux()
 	{
 		if (i_flag == FLAG_LEXICA)	addLexica();
 		else						tag();
 	}
-	
+
 	/** Called by {@link CPOSTagger#processAux()}. */
 	protected void addLexica()
 	{
@@ -289,30 +290,30 @@ public class CPOSTagger extends AbstractStatisticalComponent
 		for (i=1; i<t_size; i++)
 		{
 			node = d_tree.get(i);
-			
-			if (s_lsfs.contains(node.lowerSimplifiedForm))			
+
+			if (s_lsfs.contains(node.lowerSimplifiedForm))
 				p_ambi.add(node.simplifiedForm, g_tags[i]);
 		}
 	}
-	
+
 	/** Called by {@link CPOSTagger#processAux()}. */
 	protected void tag()
 	{
 		DEPNode input;
-		
+
 		for (i_input=1; i_input<t_size; i_input++)
 		{
 			input = d_tree.get(i_input);
 			input.pos = getLabel();
 		}
 	}
-	
+
 	/** Called by {@link CPOSTagger#tag()}. */
 	protected String getLabel()
 	{
 		StringFeatureVector vector = getFeatureVector(f_xmls[0]);
 		String label = null;
-		
+
 		if (i_flag == FLAG_TRAIN)
 		{
 			label = getGoldLabel();
@@ -322,46 +323,190 @@ public class CPOSTagger extends AbstractStatisticalComponent
 		{
 			label = getAutoLabel(vector);
 		}
-		
+
 		return label;
 	}
-	
+
 	/** Called by {@link CPOSTagger#getLabel()}. */
 	private String getGoldLabel()
 	{
 		return g_tags[i_input];
 	}
-	
+
 	/** Called by {@link CPOSTagger#getLabel()}. */
 	private String getAutoLabel(StringFeatureVector vector)
 	{
 		StringPrediction p = s_models[0].predictBest(vector);
 		return p.label;
 	}
-	
+
 //	====================================== NODE GETTER ======================================
-	
+
 	/** @return a node specified by the feature token. */
 	protected DEPNode getNodeInput(FtrToken token)
 	{
 		int index = i_input + token.offset;
 		return (0 < index && index < t_size) ? d_tree.get(index) : null;
 	}
-	
-	}
-	
-//	====================================== FEATURE EXTRACTION ======================================
-	
-	@Override
-	protected String[] getFields(FtrToken token)
+
+	/** @return a feature vector using the specific feature template. */
+	protected StringFeatureVector getFeatureVector(JointFtrXml xml)
 	{
-		return null;
+		StringFeatureVector vector = new StringFeatureVector();
+
+		for (FtrTemplate template : xml.getFtrTemplates())
+			addFeatures(vector, template);
+
+		return vector;
 	}
-	
-	@Override
+
+	/** Called by {@link AbstractStatisticalComponent#getFeatureVector(JointFtrXml)}. */
+	private void addFeatures(StringFeatureVector vector, FtrTemplate template)
+	{
+		FtrToken[] tokens = template.tokens;
+		int i, size = tokens.length;
+
+		if (template.isSetFeature())
+		{
+			String[][] fields = new String[size][];
+			String[]   tmp;
+
+			for (i=0; i<size; i++)
+			{
+				tmp = getFields(tokens[i]);
+				if (tmp == null)	return;
+				fields[i] = tmp;
+			}
+
+			addFeatures(vector, template.type, fields, 0, "");
+		}
+		else
+		{
+			StringBuilder build = new StringBuilder();
+			String field;
+
+			for (i=0; i<size; i++)
+			{
+				field = getField(tokens[i]);
+				if (field == null)	return;
+
+				if (i > 0)	build.append(AbstractColumnReader.BLANK_COLUMN);
+				build.append(field);
+			}
+
+			vector.addFeature(template.type, build.toString());
+		}
+    }
+
+	/** Called by {@link AbstractStatisticalComponent#getFeatureVector(JointFtrXml)}. */
+	private void addFeatures(StringFeatureVector vector, String type, String[][] fields, int index, String prev)
+	{
+		if (index < fields.length)
+		{
+			for (String field : fields[index])
+			{
+				if (prev.isEmpty())
+					addFeatures(vector, type, fields, index+1, field);
+				else
+					addFeatures(vector, type, fields, index+1, prev + AbstractColumnReader.BLANK_COLUMN + field);
+			}
+		}
+		else
+			vector.addFeature(type, prev);
+	}
+
 	protected String getField(FtrToken token)
 	{
+		DEPNode node = getNodeInput(token);
+		if (node == null)	return null;
+		Matcher m;
+
+		if (token.isField(JointFtrXml.F_SIMPLIFIED_FORM))
+		{
+			return (s_lsfs.contains(node.lowerSimplifiedForm)) ? node.simplifiedForm : null;
+		}
+		else if (token.isField(JointFtrXml.F_LOWER_SIMPLIFIED_FORM))
+		{
+			return (s_lsfs.contains(node.lowerSimplifiedForm)) ? node.lowerSimplifiedForm : null;
+		}
+		else if (token.isField(JointFtrXml.F_POS))
+		{
+			return node.pos;
+		}
+		else if (token.isField(JointFtrXml.F_AMBIGUITY_CLASS))
+		{
+			return m_ambi.get(node.simplifiedForm);
+		}
+		else if ((m = JointFtrXml.P_BOOLEAN.matcher(token.field)).find())
+		{
+			int field = Integer.parseInt(m.group(1));
+
+			switch (field)
+			{
+			case  0: return UTString.isAllUpperCase(node.simplifiedForm) ? token.field : null;
+			case  1: return UTString.isAllLowerCase(node.simplifiedForm) ? token.field : null;
+			case  2: return UTString.beginsWithUpperCase(node.simplifiedForm) ? token.field : null;
+			case  3: return UTString.getNumOfCapitalsNotAtBeginning(node.simplifiedForm) == 1 ? token.field : null;
+			case  4: return UTString.getNumOfCapitalsNotAtBeginning(node.simplifiedForm)  > 1 ? token.field : null;
+			case  5: return node.simplifiedForm.contains(".") ? token.field : null;
+			case  6: return UTString.containsDigit(node.simplifiedForm) ? token.field : null;
+			case  7: return node.simplifiedForm.contains("-") ? token.field : null;
+			case  8: return (i_input == t_size-1) ? token.field : null;
+			case  9: return (i_input == 1) ? token.field : null;
+			default: throw new IllegalArgumentException("Unsupported feature: "+field);
+			}
+		}
+		else if ((m = JointFtrXml.P_FEAT.matcher(token.field)).find())
+		{
+			return node.getFeat(m.group(1));
+		}
+		else if ((m = JointFtrXml.P_PREFIX.matcher(token.field)).find())
+		{
+			int n = Integer.parseInt(m.group(1)), len = node.lowerSimplifiedForm.length();
+			return (n <= len) ? node.lowerSimplifiedForm.substring(0, n) : null;
+		}
+		else if ((m = JointFtrXml.P_SUFFIX.matcher(token.field)).find())
+		{
+			int n = Integer.parseInt(m.group(1)), len = node.lowerSimplifiedForm.length();
+			return (n <= len) ? node.lowerSimplifiedForm.substring(len-n, len) : null;
+		}
+
 		return null;
 	}
 
+	protected String[] getFields(FtrToken token)
+	{
+		DEPNode node = getNodeInput(token);
+		if (node == null)	return null;
+		Matcher m;
+
+		if ((m = JointFtrXml.P_PREFIX.matcher(token.field)).find())
+		{
+			String[] fields = UTString.getPrefixes(node.lowerSimplifiedForm, Integer.parseInt(m.group(1)));
+			return fields.length == 0 ? null : fields;
+		}
+		else if ((m = JointFtrXml.P_SUFFIX.matcher(token.field)).find())
+		{
+			String[] fields = UTString.getSuffixes(node.lowerSimplifiedForm, Integer.parseInt(m.group(1)));
+			return fields.length == 0 ? null : fields;
+		}
+
+		return null;
+	}
+
+	}
+
+//	====================================== FEATURE EXTRACTION ======================================
+
+	@Override
+	protected String[] getFields(FtrToken token)
+	{
+		throw new IllegalArgumentException();
+	}
+
+	@Override
+	protected String getField(FtrToken token)
+	{
+		throw new IllegalArgumentException();
+	}
 }
